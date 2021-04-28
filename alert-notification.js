7b275e9ce8677942952e313b17973be6d5b7e2bc
neil mclaughlin <neil.mclaughlin@defra.gov.uk>
2021-04-28 17:21:38 +0100

    Create/update campaign for alert

--- alert-notification.js
+++ alert-notification.js
@@ -1,4 +1,35 @@
-// const AWS = require('aws-sdk')
+const AWS = require('aws-sdk')
+const pinpointApplicationId = process.env.PINPOINT_APPLICATION_ID
+const pinpoint = new AWS.Pinpoint()
+const campaignTemplate = require('./campaign-template.json')
+const parse = require('json-templates')
+
+async function createCampaign (areaCode, headline, description) {
+  const template = parse(campaignTemplate)
+
+  const body = `<!DOCTYPE html><html lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=utf-8"/></head><body><p>${description}</p></body></html>`
+
+  const params = template({
+    applicationId: pinpointApplicationId,
+    headline,
+    body,
+    name: areaCode,
+    segmentId: '31a8bccb8cfe4f48a0184ef2e932cbf7',
+    segmentVersion: 1
+  })
+
+  console.log(JSON.stringify(params, null, 4))
+
+  try {
+    const pinpointResult = await pinpoint.createCampaign(params).promise()
+    console.log({ error: false, pinpointResult })
+    return {
+      pinpointResult
+    }
+  } catch (err) {
+    console.log({ error: true, err })
+  }
+}
 
 async function handler (event) {
   // console.log({ event })
@@ -15,6 +46,7 @@ async function handler (event) {
   // get alert url from event
   // get alert from s3
   // create campaign for target area
+  await createCampaign(message.area_code, message.headline, message.description)
 }
 
 module.exports = { handler }
