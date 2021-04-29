1bce125d74294d49e98965f92c58f7941e45d7ad
neil mclaughlin <neil.mclaughlin@defra.gov.uk>
2021-04-29 14:36:20 +0100

    Create SMS campaign

--- alert-notification.js
+++ alert-notification.js
@@ -1,7 +1,8 @@
 const AWS = require('aws-sdk')
 const pinpointApplicationId = process.env.PINPOINT_APPLICATION_ID
 const pinpoint = new AWS.Pinpoint()
-const campaignTemplate = require('./campaign-template.json')
+const emailCampaignTemplate = require('./email-campaign-template.json')
+const smsCampaignTemplate = require('./sms-campaign-template.json')
 const segmentTemplate = require('./segment-template.json')
 const parse = require('json-templates')
 
@@ -22,8 +23,8 @@ async function createSegment (areaCode) {
   }
 }
 
-async function createCampaign (segmentId, segmentVersion, areaCode, headline, description) {
-  const template = parse(campaignTemplate)
+async function createEmailCampaign (segmentId, segmentVersion, areaCode, headline, description) {
+  const template = parse(emailCampaignTemplate)
 
   const body = `<!DOCTYPE html><html lang="en"><head><meta http-equiv="Content-Type" content="text/html; charset=utf-8"/></head><body><p>${description}</p></body></html>`
 
@@ -31,6 +32,30 @@ async function createCampaign (segmentId, segmentVersion, areaCode, headline, de
     applicationId: pinpointApplicationId,
     headline,
     body,
+    name: areaCode,
+    segmentId,
+    segmentVersion
+  })
+
+  console.log(JSON.stringify(params, null, 4))
+
+  try {
+    const pinpointResult = await pinpoint.createCampaign(params).promise()
+    console.log({ error: false, pinpointResult })
+    return {
+      pinpointResult
+    }
+  } catch (err) {
+    console.log({ error: true, err })
+  }
+}
+
+async function createSmsCampaign (segmentId, segmentVersion, areaCode, headline, description) {
+  const template = parse(smsCampaignTemplate)
+
+  const params = template({
+    applicationId: pinpointApplicationId,
+    body: `${headline}\n${description}`,
     name: areaCode,
     segmentId,
     segmentVersion
@@ -61,7 +86,11 @@ async function handler (event) {
   console.log({ message })
 
   const { id, version } = await createSegment(message.area_code)
-  await createCampaign(id, version, message.area_code, message.headline, message.description)
+  const [emailResult, smsResult] = await Promise.all([
+    createEmailCampaign(id, version, message.area_code, message.headline, message.description),
+    createSmsCampaign(id, version, message.area_code, message.headline, message.description)
+  ])
+  console.log({ emailResult, smsResult })
 }
 
 module.exports = { handler }
